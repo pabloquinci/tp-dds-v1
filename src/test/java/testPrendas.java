44e7784756a1de8813f00944f63a60cb13569d17
Pablo Quinci <pablo.quinci@gmail.com>
2019-03-29 00:42:19 -0300

    Modificacion test

--- src/test/java/testPrendas.java
+++ src/test/java/testPrendas.java
@@ -12,14 +12,16 @@ public class testPrendas {
 		PrendaPies prendaPies1=new PrendaPies("Zapatos Gamuza", TipoPrenda.PIES);
 		PrendaTorso prendaTorso1=new PrendaTorso("Remera QuickSilver", TipoPrenda.TORSO);
 		PrendaPiernas prendaPierna1=new PrendaPiernas("Pantalon Levis", TipoPrenda.PIERNA);
-		//Accesorio accesorio1=new Accesorio("Pulsera", TipoPrenda.ACCESORIO);
+		Accesorio accesorio1=new Accesorio("Pulsera", TipoPrenda.ACCESORIO);
 
 
 		persona1.getPrendas().add(prendaPies1);
 		persona1.getPrendas().add(prendaTorso1);
 		persona1.getPrendas().add(prendaPierna1);
+		persona1.getPrendas().add(accesorio1);
 
-		Assert.assertEquals(false, persona1.esSugerenciaValida());
+
+		Assert.assertEquals(true, persona1.esSugerenciaValida());
 
 	}
 
